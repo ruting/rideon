382ee52a08bb5f4a29a2edcd293c0fa59e45fc56
ruting <liuruting@gmail.com>
2013-04-03 06:51:07 +0800

    First commit with raw files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rideon::Application.config.secret_token = 'ca3e937fbecb7b21a76fced0ee2974f5240fbe670f462fb31290f882a915599d4d47ac051608f62869ef06a709713ecf8d3d01abfbd041706f30e1e12d242d30'
