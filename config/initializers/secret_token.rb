ea77272ff2ef34a0f8a6ab46c4b85d1513878ea2
ruting <liuruting@gmail.com>
2013-04-03 08:31:06 +0800

    install twitter boostrap

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rideon::Application.config.secret_token = 'ca3e937fbecb7b21a76fced0ee2974f5240fbe670f462fb31290f882a915599d4d47ac051608f62869ef06a709713ecf8d3d01abfbd041706f30e1e12d242d30'
+Rideon::Application.config.secret_token = '4ff1ee494c410df7e354e0fe049366e34df5228b3b93c64a679b91d246a8e3f665414341d8c6ecf559b5d154c8b0de1ca597e4eff5c062e0afef3341d4b8213f'
